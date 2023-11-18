d29d6358409e83847a70073e00e6b04bd3c46709
Belal-private-repos <belalprivateacc@gmail>
2023-11-18 08:27:04 +0200

    Finished developing the AboutComponent

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import { Skill } from '../skill';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent {
-
+export class AboutComponent implements OnInit {
+  skills: Skill[] = [
+    { skillName: 'HTML', skillValue: '85%' },
+    { skillName: 'CSS3', skillValue: '75%' },
+    { skillName: 'PHP', skillValue: '50%' },
+    { skillName: 'JAVASCRIPT', skillValue: '90%' },
+  ];
+  ngOnInit(): void {}
 }
