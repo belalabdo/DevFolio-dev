44f88cd020ea4076c438f8fe7bf55161a1da7ad8
Belal-private-repos <belalprivateacc@gmail>
2023-11-18 02:30:13 +0200

    Created the HeaderComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { NgxTypedJsModule } from 'ngx-typed-js';
+
+// Components
 import { AppComponent } from './app/app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, NavbarComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule, NgxTypedJsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
