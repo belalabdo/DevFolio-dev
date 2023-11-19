c84f0729ffa19e63bf49781cf13d1c045d535dc8
Belal-private-repos <belalprivateacc@gmail>
2023-11-19 12:50:39 +0200

    Finished developing the counters section

--- src/app/counters/counters.component.ts
+++ src/app/counters/counters.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Counter } from '../counter';
+import { CountUpOptions } from '../count-up-options';
 
 @Component({
   selector: 'app-counters',
   templateUrl: './counters.component.html',
-  styleUrls: ['./counters.component.scss']
+  styleUrls: ['./counters.component.scss'],
 })
 export class CountersComponent {
-
+  counters: Counter[] = [
+    {
+      name: 'works completed',
+      class: 'fa-solid fa-check fs-3',
+      value: 450,
+    },
+    {
+      name: 'years of experience',
+      class: 'fa-solid fa-newspaper fs-3',
+      value: 25,
+    },
+    {
+      name: 'total clients',
+      class: 'fa-solid fa-user-group fs-3',
+      value: 550,
+    },
+    {
+      name: 'award won',
+      class: 'fa-solid fa-award fs-3',
+      value: 48,
+    },
+  ];
+  options: CountUpOptions = { enableScrollSpy: true, duration: 1.5 };
 }
