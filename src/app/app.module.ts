48872932e0309bb8053dc71ab7da32ef93f5dceb
Belal-private-repos <belalprivateacc@gmail>
2023-11-19 11:34:49 +0200

    Added the CountUp lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { NgxTypedJsModule } from 'ngx-typed-js';
+import { CountUpModule } from 'ngx-countup';
 
 // Components
 import { AppComponent } from './app/app.component';
@@ -12,8 +13,15 @@ import { ServicesComponent } from './services/services.component';
 import { CountersComponent } from './counters/counters.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HeaderComponent, AboutComponent, ServicesComponent, CountersComponent],
-  imports: [BrowserModule, AppRoutingModule, NgxTypedJsModule],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    AboutComponent,
+    ServicesComponent,
+    CountersComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, NgxTypedJsModule, CountUpModule],
   providers: [],
   bootstrap: [AppComponent],
 })
